72bda1e81423ad75d3a6de0c5e5ad17342de6c5e
Onel Harrison <onelharrison@users.noreply.github.com>
2019-03-10 20:17:10 -0400

    Add to gitignore and git.zsh

--- bin/git_setup.sh
+++ bin/git_setup.sh
@@ -8,7 +8,10 @@ mv $HOME/.gitconfig $HOME/.gitconfig.old 2>/dev/null
 ln -s $SCRIPT_ABS_DIR/conf/git/gitconfig ~/.gitconfig
 
 mv $HOME/.gitignore $HOME/.gitignore.old 2>/dev/null
-ln -s $SCRIPT_ABS_DIR/conf/git/gitignore ~/.gitignore
+ln -s $SCRIPT_ABS_DIR/conf/git/gitignore_global ~/.gitignore_global
+
+git config --global core.excludesfile ~/.gitignore_global
+
 
 echo 'DONE: git setup completed.'
 echo '--------'
