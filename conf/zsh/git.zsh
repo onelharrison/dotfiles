887bae8c44d89c148444b82873bbb528a6bd4516
Onel Harrison <onelharrison@users.noreply.github.com>
2019-03-10 20:17:10 -0400

    Add to gitignore and git.zsh

---
+++ conf/zsh/git.zsh
@@ -0,0 +1,24 @@
+alias g='git'
+
+alias ga='git add'
+
+alias gb='git branch'
+alias gbd='git branch -d'
+alias gbD='git branch -D'
+
+alias gc='git commit'
+alias gcb='git checkout -b'
+alias gcm='git checkout master'
+alias gcmsg='git commit -m'
+alias gco='git checkout'
+
+alias gd='git diff'
+
+alias gf='git fetch'
+
+alias ggpull='git pull origin "$(git_current_branch)"'
+alias ggpush='git push origin "$(git_current_branch)"'
+
+alias gm='git merge'
+
+alias gst='git status'
