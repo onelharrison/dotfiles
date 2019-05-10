8f48e219a0acbe5fb9cfcdef1c60a75bbe2adcf3
Onel <onel.harrison@listenfirstmedia.com>
2019-05-10 20:56:39 +0000

    Add git repo space

--- bin/git_setup.sh
+++ bin/git_setup.sh
@@ -2,16 +2,22 @@
 
 echo 'INIT: git setup initiated.'
 
+export GIT_USER='onelharrison'
+export GIT_HOME="$HOME/github.com/$GIT_USER"
+
 check_or_install git
 
+# Setup space for git repos
+mkdir -p  $HOME/github.com
+mkdir -p  $HOME/github.com/$GIT_USER
+
+# Setup global gitconfig
 mv $HOME/.gitconfig $HOME/.gitconfig.old 2>/dev/null
 ln -s $SCRIPT_ABS_DIR/conf/git/gitconfig ~/.gitconfig
 
+# Setup global gitignore
 mv $HOME/.gitignore $HOME/.gitignore.old 2>/dev/null
 ln -s $SCRIPT_ABS_DIR/conf/git/gitignore_global ~/.gitignore_global
-
-git config --global core.excludesfile ~/.gitignore_global
-
 
 echo 'DONE: git setup completed.'
 echo '--------'
