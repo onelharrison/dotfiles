5d819dbae9c3ae0985c5787b0ae87055d29837d5
Onel Harrison <onelharrison@users.noreply.github.com>
2019-05-24 21:20:19 -0400

    Add emacs configurations for evil mode

---
+++ bin/emacs_setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+echo 'INIT: emacs setup initiated.'
+
+check_or_install emacs
+
+if [ -d ~/.emacs.d.old ]; then
+  rm -r ~/.emacs.d.old
+fi
+
+mv $HOME/.emacs $HOME/.emacs.old 2>/dev/null
+ln -s $SCRIPT_ABS_DIR/conf/emacs/emacs $HOME/.emacs
+
+# install evil mode
+if [ -e $HOME/.emacs.d.old ]; then
+  rm -rf $HOME/.emacs.d.old
+fi
+mv $HOME/.emacs.d $HOME/.emacs.old 2>/dev/null
+ln -s $SCRIPT_ABS_DIR/conf/emacs/emacs.d $HOME/.emacs.d 2>/dev/null
+
+echo 'DONE: emacs setup completed.'
+echo '--------'
+
