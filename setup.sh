070f9ede4ef0d8fe1cffeca11e733c9733b5336e
Onel Harrison <onelharrison@users.noreply.github.com>
2019-07-19 19:30:17 -0400

    ls

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ export SCRIPT_ABS_DIR=$(dirname $SCRIPT_ABS_PATH)
 . $SCRIPT_ABS_DIR/lib/utils.sh
 
 # Core
+. $SCRIPT_ABS_DIR/bin/ag_setup.sh
 . $SCRIPT_ABS_DIR/bin/git_setup.sh
 . $SCRIPT_ABS_DIR/bin/tmux_setup.sh
 . $SCRIPT_ABS_DIR/bin/vim_setup.sh
