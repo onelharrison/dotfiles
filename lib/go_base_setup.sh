3da6ca1455ce70273958af72f2137a7613de7ce1
Onel Harrison <onelharrison@users.noreply.github.com>
2020-05-22 22:48:27 -0400

    Specify shells for ggovm to configured for

--- lib/go_base_setup.sh
+++ lib/go_base_setup.sh
@@ -3,7 +3,7 @@
 install_g() {
   echo 'INFO: Installing g'
 
-  curl -sSL https://git.io/g-install | sh -s
+  curl -sSL https://git.io/g-install | sh -s -- bash zsh
 
   echo 'INFO: Done installing g'
 }
