2efaa8b2cccb4eaaf44b5da866ec4fa117ea3a65
Onel Harrison <onelharrison@users.noreply.github.com>
2021-03-13 21:05:14 -0500

    Update aws setup script

--- lib/aws_setup.sh
+++ lib/aws_setup.sh
@@ -2,15 +2,20 @@
 
 echo 'INIT: awscli setup initiated.'
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+INSTALL_DIR="$SCRIPT_ABS_DIR/tmp/awscli"
+mkdir -p $INSTALL_DIR
 
-rm -rf awscliv2.zip
-rm -rf ./aws
-
-rm -rf $HOME/.aws
-ln -s $SCRIPT_ABS_DIR/conf/aws $HOME/.aws
+# Install awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" \
+	-o "$INSTALL_DIR/awscliv2.zip" && \
+	cd $INSTALL_DIR && \
+	unzip awscliv2.zip && \
+	sudo ./aws/install --update && \
+	aws --version && \
+	cd - && \
+        rm -rf $INSTALL_DIR && \
+        rm -rf $HOME/.aws && \
+        ln -s $SCRIPT_ABS_DIR/conf/aws $HOME/.aws
 
 echo 'DONE: awscli setup completed.'
 echo '--------'
