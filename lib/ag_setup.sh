875843d59b92721ee19cb505547cd803c35248ee
Onel Harrison <onel.harrison@renaissance.com>
2021-06-22 18:01:21 -0400

    Update pkg_name for silver search when on macos

--- lib/ag_setup.sh
+++ lib/ag_setup.sh
@@ -2,7 +2,16 @@
 
 echo 'INIT: ag setup initiated.'
 
-check_or_install silversearcher-ag
+case $OSTYPE in
+	darwin*)
+		pkg_name="the_silver_searcher"
+		;;
+	*)
+		pkg_name="silversearcher-ag"
+		;;
+esac
+
+check_or_install $pkg_name
 
 echo 'DONE: ag setup completed.'
 echo '--------'
