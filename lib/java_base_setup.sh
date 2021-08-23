e7172887fbb756d080705187de2df090aa089205
Onel Harrison <onelharrison@users.noreply.github.com>
2021-08-22 22:05:14 -0400

    Update java_base_setup

--- lib/java_base_setup.sh
+++ lib/java_base_setup.sh
@@ -5,7 +5,7 @@ echo 'INIT: java setup initiated.'
 check_or_install default-jre
 check_or_install default-jdk
 
-cat <<TEXT $SCRIPT_ABS_DIR/conf/zsh/zshrc_extended
+cat <<TEXT >> $SCRIPT_ABS_DIR/conf/zsh/zshrc_extended
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/"
 TEXT
 
