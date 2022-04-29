eeb90ff9d6f4c3ecc749de6929eb6f548344c8f9
Onel Harrison <onelharrison@users.noreply.github.com>
2022-04-29 17:08:58 -0500

    Remove pkg_install.ps1; Add setup.ps1

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,2 +1,34 @@
-# Install Chocolatey
+# Download and Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Configure WindowsOptionalFeatures
+# =================================
+
+# Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All  -NoRestart
+
+# Enable Windows Subsystem for Linux
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart
+
+# Enable Virtual Machine Platform
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -NoRestart
+
+# TODO: To be ran after restart
+#
+# Configure Windows Subsystem for Linux
+# =====================================
+
+# TODO: Download and run the Linux kernel update package (for x64 arch)
+# https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
+
+# Set WSL default version to v2
+# wsl --set-default-version 2
+
+# Install Ubuntu 20.04 LTS WSL distro
+# wsl --install -d Ubuntu-20.04
+
+# Configure Services
+# ==================
+
+# Start the ssh-agent service manually when the ssh-agent command is invoked
+Get-Service -Name ssh-agent | Set-Service -StartupType Manual
