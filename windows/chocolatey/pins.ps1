5137581943585f6cce9fcf77181c975343e433a3
Onel Harrison <onelharrison@users.noreply.github.com>
2022-05-08 13:58:59 -0400

    Pin syncthing version for chocolatey install

--- windows/chocolatey/pins.ps1
+++ windows/chocolatey/pins.ps1
@@ -1,3 +1,4 @@
 choco pin add --name="'calibre'" --version="'4.23.0'"
 choco pin add --name="'foxitreader'" --version="'11.1.0.52543'"
 choco pin add --name="'virtualbox'" --version="'6.1.26'"
+choco pin add --name="'syncthing'" --version="'1.19.2'"
